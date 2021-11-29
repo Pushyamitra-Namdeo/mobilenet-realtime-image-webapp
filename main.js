21a2d83a7091e3c276190f822ab14a9e77aff1ba
Pushyamitra-Namdeo <79986813+Pushyamitra-Namdeo@users.noreply.github.com>
2021-11-29 17:40:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,27 @@ function setup() {
   canvas.center();
   video = createCapture(VIDEO);
   video.hide();
+  classifier= ml5.imageClassifier('MobileNet',model_loaded);
 }
 
+function draw(){
+image(video,0,0,300,300);
+classifier.classify(video,got_results);
+}
+
+function model_loaded(){
+console.log("Model Loaded!");
+}
+
+function got_results(error,results){
+if(error){ 
+  console.error(error)
+}else{
+  console.log(results)
+  document.getElementById("result_object_name").innerHTML= results[0].label;
+  document.getElementById("result_accuracy_name").innerHTML= results[0].confidence.toFixed(3);
+}
+
+}
 
 
